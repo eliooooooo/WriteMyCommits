b13f67a78a8b8c356e06d3f1d0fe9446ddee7721
eliooooooo <eliott.burkle@gmail.com>
2024-03-31 11:48:14 +0200

    Feat(commits)!: allow user to commit from extension interface

--- src/extension.js
+++ src/extension.js
@@ -69,10 +69,20 @@ class MyDataProvider {
 
 			const item3 = new vscode.TreeItem('');
 
+			const item4 = new vscode.TreeItem('Commit Staged files ');
+			item4.iconPath = new vscode.ThemeIcon('play');
+			item4.command = {
+				command: 'wmc.commit',
+				title: 'Commit Staged files',
+				arguments: []
+			};
+
+			const item5 = new vscode.TreeItem('');
+
             if (!this.workspaceRoot) {
                 vscode.window.showInformationMessage('No data');
-				const item4 = new vscode.TreeItem('No data, please try to open a git repository');
-                return Promise.resolve([item1, item2, item3, item4]);
+				const item6 = new vscode.TreeItem('No data, please try to open a git repository');
+                return Promise.resolve([item1, item2, item3, item4, item5, item6]);
             }
 
 			const stagedFilesPromise = new Promise((resolve, reject) => {
@@ -99,7 +109,7 @@ class MyDataProvider {
 				});
 			});
 			
-			return Promise.all([Promise.resolve(item1), Promise.resolve(item2), Promise.resolve(item3), stagedFilesPromise, modifiedFilesPromise]);
+			return Promise.all([Promise.resolve(item1), Promise.resolve(item2), Promise.resolve(item3), Promise.resolve(item4), Promise.resolve(item5),stagedFilesPromise, modifiedFilesPromise]);
         }
     }
 }
@@ -113,19 +123,21 @@ function activate(context) {
 		myDataProvider.refresh();
 	});
 
+	
 	const getUserInputCommand = vscode.commands.registerCommand('writemycommits.getUserInput', async () => {
 		const type = await vscode.window.showQuickPick(['Feat', 'Build', 'Chore', 'Fix', 'Docs', 'Style', 'Ci', 'Refactor', 'Perf', 'Test'], { placeHolder: 'Select commit type' });
 		let isBreakingChange = await vscode.window.showQuickPick(['Yes', 'No'], { placeHolder: 'Is it a breaking change?' });
 		const scope = await vscode.window.showInputBox({ prompt: 'Enter commit scope (optional)' });
 		const description = await vscode.window.showInputBox({ prompt: 'Enter commit description' });
 		isBreakingChange === 'Yes' ? isBreakingChange = true : isBreakingChange = false;
-
+		
 		const commitMessage = `${type}${scope ? `(${scope})` : ''}${isBreakingChange ? '!' : ''}: ${description}`;
 		await vscode.env.clipboard.writeText(commitMessage);
-
+		
 		vscode.window.showInformationMessage('Commit name copied to clipboard');
+		return commitMessage;
 	});
-
+	
 	const getUserInputFullCommand = vscode.commands.registerCommand('writemycommits.getUserInputFull', async () => {
 		const type = await vscode.window.showQuickPick(['Feat', 'Fix', 'Docs', 'Style', 'Refactor', 'Test'], { placeHolder: 'Select commit type' });
 		let isBreakingChange = await vscode.window.showQuickPick(['Yes', 'No'], { placeHolder: 'Is it a breaking change?' });	
@@ -134,11 +146,23 @@ function activate(context) {
 		const body = await vscode.window.showInputBox({ prompt: 'Enter commit body (optional)' });
 		const footer = await vscode.window.showInputBox({ prompt: 'Enter commit footer (optional)' });
 		isBreakingChange === 'Yes' ? isBreakingChange = true : isBreakingChange = false;
-
+		
 		const commitMessage = `${type}${scope ? `(${scope})` : ''}${isBreakingChange ? '!' : ''}: ${description}${body ? `\n\n${body}` : ''}${footer ? `\n\n${isBreakingChange ? 'BREAKING CHANGE : ' : ''}${footer}` : ''}`;
 		await vscode.env.clipboard.writeText(commitMessage);
-
+		
 		vscode.window.showInformationMessage('Commit message copied to clipboard');
+	});
+	
+	const commitCommand = vscode.commands.registerCommand('wmc.commit', async () => {
+		const message = await vscode.commands.executeCommand('writemycommits.getUserInput');
+		cp.exec('git commit -m"' + String(message) + '"', { cwd: vscode.workspace.workspaceFolders[0].uri.fsPath }, (err, stdout, stderr) => {
+			if (err) {
+				console.error(err);
+				vscode.window.showErrorMessage('Error: ' + stderr);
+			} else {
+				vscode.window.showInformationMessage('Commit successful');
+			}
+		})
 	});
 
 	const docCommand = vscode.commands.registerCommand('writemycommits.getDoc', async () => {
@@ -147,6 +171,7 @@ function activate(context) {
 
 	context.subscriptions.push(getUserInputCommand);
 	context.subscriptions.push(getUserInputFullCommand);
+	context.subscriptions.push(commitCommand);
 	context.subscriptions.push(docCommand);
 }
 
